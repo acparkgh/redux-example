69d76462ec08231f403573bcbb37ead3442b6544
acparkgh <acpark123@gmail.com>
2021-03-02 00:55:52 -0500

    created Reducer and Store

--- redux-basics.js
+++ redux-basics.js
@@ -25,6 +25,11 @@ const rootReducer = (state = initialState, action) => {
 const store = createStore(rootReducer);
 console.log(store.getState());
 
+//Subscription
+
+store.subscribe(() => {
+  console.log('[Subscription]', store.getState())
+});
 
 // Dispatching Action
 
@@ -32,6 +37,3 @@ store.dispatch({ type: 'INC_COUNTER' })
 store.dispatch({ type: 'ADD_COUNTER', value: 10 })
 console.log(store.getState());
   
-
-
-//Subscription
