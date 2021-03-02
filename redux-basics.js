8db74162d2f55b0fd8f695824d676cf816d9da05
acparkgh <acpark123@gmail.com>
2021-03-01 23:57:08 -0500

    Running Redux with node.js

---
+++ redux-basics.js
@@ -0,0 +1,20 @@
+
+const redux = require('redux');
+const createStore = redux.createStore;
+
+// Reducer
+const rootReducer = (store, action) => {
+  return (
+    state
+  ) ;
+};
+
+// Store
+const store = createStore(rootReducer);
+
+
+// Dispatching Action
+
+
+
+//Subscription
