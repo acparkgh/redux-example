6c1cf7212a299dc3cf9a93f71601c9a220b0b33c
acparkgh <acpark123@gmail.com>
2021-03-02 00:37:05 -0500

    added Action

--- redux-basics.js
+++ redux-basics.js
@@ -8,9 +8,17 @@ const initialState = {
 
 // Reducer
 const rootReducer = (state = initialState, action) => {
-  return (
-    state
-  );
+  if(action.type === 'INC_COUNTER') {
+    return (
+      { ...state, counter: state.counter + 1 }
+    );
+  }
+  if(action.type === 'ADD_COUNTER') {
+    return (
+      { ...state, counter: state.counter + action.value }
+    );
+  }
+  return state;
 };
 
 // Store
@@ -20,6 +28,10 @@ console.log(store.getState());
 
 // Dispatching Action
 
+store.dispatch({ type: 'INC_COUNTER' })
+store.dispatch({ type: 'ADD_COUNTER', value: 10 })
+console.log(store.getState());
+  
 
 
 //Subscription
